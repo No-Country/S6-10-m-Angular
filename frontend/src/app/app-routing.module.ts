57bee89c7bc4407daff9cb91c5f8590db5b43fda
Santiago <monitocastellani@gmail.com>
2023-02-21 16:47:53 -0300

    fix:deploy

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component'
-import { LandingComponent } from './components/landing/landing.component'
-import { PatientDashboardComponent } from './components/patient-dashboard/patient-dashboard.component'
-import { TurnosSearchComponent } from './modules/user/components/turnos/turnos-search.component'
-import { TurnosComponent } from './modules/user/components/turnos/turnos.component'
-import { DashboardComponent } from './modules/user/dashboard/dashboard.component'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { PatientDashboardComponent } from './components/patient-dashboard/patient-dashboard.component';/*
+import { TurnosSearchComponent } from './modules/user/components/turnos/turnos-search.component';
+import { TurnosComponent } from './modules/user/components/turnos/turnos.component';
+import { DashboardComponent } from './modules/user/dashboard/dashboard.component';*/
 
 const routes: Routes = [
 	{path:'home', component:LandingComponent},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http'
-import { AuthModule } from './modules/auth/auth.module'
-import { UserModule } from './modules/user/user.module'
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { LandingComponent } from './components/landing/landing.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { PatientDashboardComponent } from './components/patient-dashboard/patient-dashboard.component';
 import { AdminModule } from './modules/admin/admin.module';
 import { DoctorModule } from './modules/doctor/doctor.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 
 @NgModule({
