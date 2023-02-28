2264d022ac562178a15cebe6dda1b872cac7acba
Santiago <monitocastellani@gmail.com>
2023-02-28 12:39:54 -0300

    feat:add links,changes styles

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { LandingComponent } from './components/landing/landing.component';
-import { PatientDashboardComponent } from './components/patient-dashboard/patient-dashboard.component';/*
-import { TurnosSearchComponent } from './modules/user/components/turnos/turnos-search.component';
-import { TurnosComponent } from './modules/user/components/turnos/turnos.component';
-import { DashboardComponent } from './modules/user/dashboard/dashboard.component';*/
+import { PatientDashboardComponent } from './components/patient-dashboard/patient-dashboard.component';
 
 const routes: Routes = [
 	{path:'home', component:LandingComponent},
-  {path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)},
+	{path:'about-us',component:AboutUsComponent},	
+  	{path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)},
 	{path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)},
 	{path: 'doctor', loadChildren: () => import('./modules/doctor/doctor.module').then(m => m.DoctorModule)},
 	{path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)},
 	{path:'admin-dashboard',component:AdminDashboardComponent},
 	{path:'patient-dashboard',component:PatientDashboardComponent},
-
 	{ path: '**', redirectTo: 'home'}
 ];
 
