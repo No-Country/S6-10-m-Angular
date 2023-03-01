8e16ad0983443a29c1af476a514332a62b83bd67
Santiago <monitocastellani@gmail.com>
2023-03-01 02:43:35 -0300

    changes2

--- frontend/src/app/modules/auth/new-password/new-password.component.ts
+++ frontend/src/app/modules/auth/new-password/new-password.component.ts
@@ -20,9 +20,16 @@ export class NewPasswordComponent implements OnInit {
       // Guardar los parámetros para su uso posterior
       this.saveParams(params);
     });
-  }  
-  saveParams(params:any){      
-    sessionStorage.setItem('a',params);
+  }
+
+  saveParams(params:any){ 
+    const transform = params.toString();     
+    sessionStorage.setItem('a',transform);
+  }
+
+  recuperar(){
+    const data = sessionStorage.getItem('a');
+    console.log(data)
   }
     
     
